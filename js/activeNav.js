361a4d38860bc944a382596e5feaa51f9f59e133
owenqing <825515396@qq.com>
2025-03-15 18:20:06 +0800

    Site updated: 2025-03-15 18:20:06

--- js/activeNav.js
+++ js/activeNav.js
@@ -1,9 +1,25 @@
 // which nav has active
 let navs = document.querySelectorAll('.nav-item');
 let pagePath = window.location.pathname;
-for(let nav of navs) {
+for (let nav of navs) {
   let navPath = nav.getAttribute("data-path");
-  if(navPath && navPath === pagePath) {
+  if (navPath && navPath === pagePath) {
     nav.className = "nav-item active";
   }
 }
+
+// Scroll effect for header
+const header = document.querySelector('.header');
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+
+  lastScroll = currentScroll;
+});
