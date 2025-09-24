ae2601dd1f33ec556a85b905b56d1b57a1391a92
owenqing <825515396@qq.com>
2025-09-24 14:06:11 +0800

    Site updated: 2025-09-24 14:06:11

--- js/activeNav.js
+++ js/activeNav.js
@@ -17,22 +17,3 @@ if (pagePath === '/') {
     }
   });
 }
-
-// 注释掉这部分代码
-/*
-// Scroll effect for header
-const header = document.querySelector('.header');
-let lastScroll = 0;
-
-window.addEventListener('scroll', () => {
-  const currentScroll = window.pageYOffset;
-
-  if (currentScroll > 50) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-
-  lastScroll = currentScroll;
-});
-*/
